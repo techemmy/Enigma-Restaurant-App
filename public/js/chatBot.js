d43144d1c27f35716a6e560a3bcc35b43ec59e89
Emmanuel Oloyede <emmanueltopea@gmail.com>
2023-03-09 23:05:42 +0100

    feat: implement function to show bot message add validation for user input add bot chat options

--- public/js/chatBot.js
+++ public/js/chatBot.js
@@ -4,12 +4,61 @@ class ChatBot {
         this.messages = messages;
         this.userInput = userInput;
         this.submitBtn = submitBtn;
+        this.chatOptions = {
+            1: "Place an Order",
+            97: "See current order",
+            98: "See order history",
+            99: "Checkout order",
+            0: "Cancel Order"
+        }
 
-        submitBtn.addEventListener('click', function() {
-            console.log(this);
+        this.showWelcomeMessage();
+        this.showChatOptions();
+
+        submitBtn.addEventListener('click', (e) => {
+           e.preventDefault();
+           const input = this.getUserInput();
+           // TODO: Use joi to validate
+           const validatedInput = this.validate(input);
+           if (validatedInput) this.processInput(message);
         })
     }
 
+    showWelcomeMessage() {
+        this.sendMessage(`Welcome ${this.username}`)
+    }
+
+    showChatOptions() {
+        let chatOptions = "Select one of the following to proceed:"
+        for (let [code, action] of Object.entries(this.chatOptions)) {
+            this.sendMessage(`\nSelect ${code} to ${action}`);
+        }
+    }
+
+    sendMessage(text, color=null) {
+        const message = document.createElement('li');
+        message.style.color = color;
+        message.textContent = text;
+        this.messages.appendChild(message);
+        window.scrollTo(0, document.body.scrollHeight);
+    }
+
+    getUserInput() {
+        return this.userInput.value;
+    }
+
+    validate(input) {
+        const validOptions = Object.keys(this.chatOptions)
+
+        if (!input.trim()) return
+        if (isNaN(input)) return this.sendMessage("Please enter a number 🙃", "red")
+        if(!validOptions.includes(input)) return this.sendMessage("Invalid option😩, Try again!", "red");
+        return parseInt(input);
+    }
+
+    processInput(message) {
+
+    }
 }
 
 export default ChatBot ;

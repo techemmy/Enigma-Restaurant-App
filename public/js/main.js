021aa5052cd5df13f3aa5e7092792b4c5e2b0782
Emmanuel Oloyede <emmanueltopea@gmail.com>
2023-03-09 23:06:08 +0100

    feat: configure chatbot with frontend

--- public/js/main.js
+++ public/js/main.js
@@ -1,2 +1,12 @@
 import ChatBot from "./chatBot.js";
-console.log(ChatBot);
+
+let username = localStorage.getItem("username");
+const messagesContainer = document.querySelector("#messages");
+const userInputBox = document.querySelector("#input");
+const submitBtn = document.querySelector("#submitBtn");
+
+while (!username || !username.trim()) {
+  username = prompt("Enter your username: ");
+}
+
+const chatBot = new ChatBot(username, messagesContainer, userInputBox, submitBtn);

c695af0e2dc123f3641adc40cfb23f5d69b4932f
Emmanuel Oloyede <emmanueltopea@gmail.com>
2023-03-11 05:14:44 +0100

    feat: implement functionality to confirm order item placement

--- public/js/chatBot.js
+++ public/js/chatBot.js
@@ -1,3 +1,5 @@
+import OrderItem from "./orderItem.js";
+
 class ChatBot {
     commands = {
         1: "Place an Order",
@@ -9,34 +11,61 @@ class ChatBot {
     states = {
         0: "CHOOSE_OPTION",
         1: "SELECT_ORDERITEM",
+        2: "TYPE_ORDERITEM_QUANTITY",
+        3: "CONFIRM_ORDERITEM"
+    }
+    confirmOrCancel = {
+        0: "CANCEL",
+        1: "CONFIRM",
     }
     constructor(user, messages, userInput, submitBtn) {
         this.state = this.states[0];
-        this.user = user;
+        this.customer = user;
         this.messages = messages;
         this.userInput = userInput;
         this.submitBtn = submitBtn;
+        this.orderItems = null;
+        this.currentOrderItem = null;
 
         this.chatOptions = this.commands;
+        this.validOptions = null;
 
-        this.showWelcomeMessage(`Welcome ${this.user.name}`);
+        this.showWelcomeMessage(`Welcome ${this.customer.name}`);
         this.showChatOptions();
 
         submitBtn.addEventListener('click', (e) => {
            e.preventDefault();
-           const message = this.getUserInput();
-           if (!this.validate(message)) return;
+           const userInput = this.getUserInput();
+           if (!this.validate(userInput)) return;
             this.clearUserInput();
            // if we're waiting for user to pick an option
            if (this.state === this.states[0]) {
-               this.sendMessage({message, user: true })
-               this.processInput(parseInt(message));
+               this.sendMessage({message: userInput, user: true })
+               this.processInput(parseInt(userInput));
            } else if (this.state === this.states[1]) {
-                const selectedItem = this.orderItems[message];
+                const selectedItem = this.orderItems[userInput];
                 this.sendMessage({
                     message: `You selected: ${selectedItem[0]}`,
                     user: true
                 })
+                this.currentOrderItem = new OrderItem(selectedItem[0], selectedItem[1]);
+                this.state = this.states[2];
+                this.sendMessage({message: "How many of these: "})
+           } else if (this.state === this.states[2]) {
+                this.currentOrderItem.quantity = userInput;
+                this.sendMessage({message: userInput});
+                this.state = this.states[3]
+                this.chatOptions = this.confirmOrCancel;
+                this.showChatOptions();
+           } else if (this.state === this.states[3]) {
+               const confirmed = this.confirmOrderPlacement(this.confirmOrCancel[userInput]);
+               if (confirmed) {
+                    this.sendMessage({message: "Order Item confirmed!"})
+                } else {
+                    this.sendMessage({message: "Order Item Cancelled!"})
+                }
+               this.resetVariables();
+               this.showChatOptions();
            }
         })
     }
@@ -50,6 +79,13 @@ class ChatBot {
         for (let [code, action] of Object.entries(this.chatOptions)) {
             this.sendMessage({message: `Select ${code} to ${action}`});
         }
+    }
+
+    resetVariables() {
+        this.chatOptions = this.commands;
+        this.state = this.states[0]
+        this.currentOrderItem = null;
+
     }
 
     sendMessage({message, backgroundColor, user}) {
@@ -69,11 +105,13 @@ class ChatBot {
     }
 
     validate(input) {
-        const validOptions = Object.keys(this.chatOptions)
+        if (this.state !== this.states[2]) {
+            this.validOptions = Object.keys(this.chatOptions)
+            if(!this.validOptions.includes(input)) return this.sendMessage({message: "Invalid option😩, Try again!"});
+        }
 
         if (!input.trim()) return
         if (isNaN(input)) return this.sendMessage({message: "Please enter a number 🙃"})
-        if(!validOptions.includes(input)) return this.sendMessage({message: "Invalid option😩, Try again!"});
         return true;
     }
 
@@ -98,11 +136,15 @@ class ChatBot {
     async placeOrder() {
         this.orderItems = JSON.parse(await this.getOrderItems());
         this.chatOptions = this.orderItems;
-        this.sendMessage({message: "Pick an item"})
-        for (let [code, item] of Object.entries(this.orderItems)) {
-            this.sendMessage({message: `Select ${code}: ${item[0]} at ${item[1]}`})
-        }
+        this.showChatOptions();
         this.state = this.states[1];
+    }
+
+    confirmOrderPlacement(value) {
+        if (value === this.confirmOrCancel[1]) {
+            this.customer.placeOrder(this.currentOrderItem);
+            return true;
+        }
     }
 
     getCurrentOrder() {
--- public/js/customer.js
+++ public/js/customer.js
@@ -1,6 +1,12 @@
 class Customer {
     constructor (name) {
         this.name = name;
+        this.currentOrder = null;
+        this.hasCurrentOrder = false;
+    }
+
+    placeOrder(orderItem) {
+        console.log(orderItem);
     }
 }
 

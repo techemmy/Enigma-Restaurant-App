238eac6bed91fcc13aabb91f3bf396ec0e925829
Emmanuel Oloyede <emmanueltopea@gmail.com>
2023-03-11 02:07:55 +0100

    feat: configure client socket handler and move chatBot operations into socket client handler (socket.js)

--- public/js/socket.js
+++ public/js/socket.js
@@ -1,1 +1,26 @@
+import ChatBot from "./chatBot.js";
+import Customer from "./customer.js";
+
+const messagesContainer = document.querySelector("#messages");
+const userInputBox = document.querySelector("#input");
+const submitBtn = document.querySelector("#submitBtn");
+
 const socket = io();
+
+document.addEventListener("DOMContentLoaded", () => {
+    let chatBot;
+    let customer;
+    socket.emit("customer:get");
+
+    socket.on("customer:create", () => {
+      let username;
+      while (!username || !username.trim()) {
+        username = prompt("Enter your username: ");
+      }
+      customer = new Customer(username);
+      chatBot = new ChatBot(username, messagesContainer, userInputBox, submitBtn);
+
+      console.log(customer);
+    });
+
+})

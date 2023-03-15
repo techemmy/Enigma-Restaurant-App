656d04b7d3a7a207839680bfa3f12e07459dbc3e
Emmanuel Oloyede <emmanueltopea@gmail.com>
2023-03-15 03:12:37 +0100

    feat: change express-session session store

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,7 +1,13 @@
 var session = require("express-session");
 const { sessionConfiguration } = require("./../config");
+const MongoStore = require('connect-mongo');
 
 const sessionMiddleware = session(sessionConfiguration);
+
+if (process.env.NODE_ENV === "production") {
+  sessionMiddleware.store = MongoStore.create({mongoUrl: process.env.MONGO_URI, dbName: "session-store"})
+  sessionMiddleware.cookie.secure = true;
+}
 
 module.exports = (app, express, io) => {
   app.set("view engine", "ejs");

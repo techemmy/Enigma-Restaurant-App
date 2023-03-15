95fe4c639efff2f1e5ee7e110bf2c80d158b0ce1
Emmanuel Oloyede <emmanueltopea@gmail.com>
2023-03-15 14:28:33 +0100

    feat: add css loader to chat and add comment to client socket.js file

--- public/js/socket.js
+++ public/js/socket.js
@@ -5,6 +5,7 @@ const messagesContainer = document.querySelector("#messages");
 const userInputBox = document.querySelector("#input");
 const submitBtn = document.querySelector("#submitBtn");
 const currencyLabel = document.querySelector("#currency");
+const loader = document.querySelector("#loader");
 
 const socket = io();
 
@@ -12,9 +13,11 @@ document.addEventListener("DOMContentLoaded", () => {
     let chatBot;
     let customer;
 
-    socket.emit("customer:get");
+    socket.emit("customer:get"); // get current user session if it exists
 
     socket.on("customer:create", () => {
+      // we create a new customer and update the server session if the user doesn't have a session already
+      loader.style.display = "none";
       let username;
       while (!username || !username.trim()) {
         username = prompt("Enter your username: ");
@@ -25,6 +28,8 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
     socket.on("customer:post", customerSession => {
+      // if the user has a session, we recreate the customer from the customer data stored in the session
+        loader.style.display = "none";
         const customerObj = customerSession.customer;
         customer = Customer.createFromSession(customerObj)
         chatBot = new ChatBot(socket, customer, messagesContainer, userInputBox, submitBtn, currencyLabel);

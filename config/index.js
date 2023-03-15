cf0e6d749f0cc561cea9e1c757208414dcd089fb
Emmanuel Oloyede <emmanueltopea@gmail.com>
2023-03-15 14:09:00 +0100

    feat: set cookie to secure

--- config/index.js
+++ config/index.js
@@ -3,9 +3,11 @@ require('dotenv').config();
 ONEDAY = 1000 * 60 * 60 * 24;
 const sessionConfiguration = {
     secret: process.env.SECRET,
+    name: "EnigmaPizza",
     resave: false,
     saveUninitialized: true,
-    cookie: { maxAge: ONEDAY }
+    proxy: true,
+    cookie: { httpOnly: true, maxAge: ONEDAY, secure: process.env.NODE_ENV === "production" }
   };
 
 module.exports = {
